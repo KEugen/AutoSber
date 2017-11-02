0cf86485e7dc119a53632610eaf8a330d7aedc88
Eugen Kharlamov <xamuron@gmail.com>
2017-11-02 17:21:58 +0300

    02112017

--- java/SiteTestClasses/CardsPage.java
+++ java/SiteTestClasses/CardsPage.java
@@ -4,35 +4,198 @@ import Helper.AdditionalMethods;
 import Helper.SetDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CardsPage extends SetDriver {
     AdditionalMethods methods = new AdditionalMethods();
+    Actions actions = new Actions(driver);
+    //card list
+    private By cardList = By.cssSelector(".g-tab_sber > div:nth-child(3) > div > div");
+    // первый фонд
+    private By firstFaundOnList = By.cssSelector(".g-tab_sber > div:nth-child(3) > div > div > a:nth-child(1)");
+    // i-й фонд
+    private By hoveredFaundOnList;
+    //чекбокс-средечко (только для первой карточки!!!)
+    private By hearthCheckbox = By.cssSelector(".g-tab_sber > div:nth-child(3) > div > div > a:nth-child(1) > div > div:nth-child(3) > div:nth-child(2)");
+    // кнопка "Добавить фонд"/"Убрать фонд" (или направление)
+    private By addCardButton = By.cssSelector(".b-card-page__card-button > div > span");
 
-    // страница карточки
+    // первое направление на странице фонда
+    private By firstDirection = By.cssSelector(".b-card-list__cards > a:nth-child(1)");
+    // выбранный фонд (selected)
+    private By selectedFaund = By.cssSelector(".b-card.b-card_selected");
+
     private By addDirectionButton = By.cssSelector(".b-card-page__card-button");
     //все фонды данного направления (n сестринских элементов)
     private By cardContent = By.cssSelector(".b-card__content");
     // плашка при добаленных фондах
     private By cardsCountOnHeader = By.cssSelector(".b-funds-cart__text");
-    private By cardsCountOnLogo = By.cssSelector(".b-header__left-column > span");
+    private By cardsCountOnLogo = By.cssSelector(".b-header__funds-counter");
     private By goOnPopup = By.cssSelector(".b-action-popup-block_sber > div:nth-child(3)");
 
+    private String firstFaundsId;
+    private String selectedFaundId;
+    private String firstDirectionId;
 
-    public boolean clickOnAddButton() {
-        List<WebElement> cards = driver.findElements(cardContent);
-        //int cardsCount = cards.size();
-        //System.out.println(cards.size());
-        driver.findElement(addDirectionButton).click();
-        methods.Wait(1000);
-        driver.findElement(goOnPopup).click();
-        String expectedlCounts = driver.findElement(cardsCountOnHeader).getText();
-        String expectedCountsOnLogo = driver.findElement(cardsCountOnLogo).getText();
-        String actualCounts = " " + cards.size() + " " + "фонд";
-        String actualCountsOnLogo = String.valueOf(cards.size());
-        return (expectedCountsOnLogo.contains(actualCountsOnLogo) && expectedlCounts.contains(actualCounts));
+    private String lastCardId;
+
+    public String getLastCardId() {
+        return lastCardId;
     }
+
+    public void setLastCardId(String id) {
+        lastCardId = id;
+    }
+
+    public boolean idIsMatched(String firstId, String secondId) {
+        if (firstId.endsWith(secondId)) return true;
+        return false;
+    }
+
+    private String getLocatorForCard(int cardNumber) {
+        return ".g-tab_sber > div:nth-child(3) > div > div > a:nth-child" + "(" + cardNumber + ")";
+    }
+
+    public void hoverOnCard(int cardNum) {
+        hoveredFaundOnList = By.cssSelector(getLocatorForCard(cardNum));
+        actions.moveToElement(driver.findElement(hoveredFaundOnList)).perform();
+    }
+
+    public void clickOnCard(int cardNum) {
+        WebElement cardElement = driver.findElement(By.cssSelector(getLocatorForCard(cardNum)));
+        setLastCardId(cardElement.getAttribute("href"));
+        //lastCardId = cardElement.getAttribute("href");
+        cardElement.click();
+    }
+
+    // проверяем ховер для первых двух карточек
+    public boolean checkHoverOnFaund() {
+        By lastHoveredCard;
+        boolean firstHoverCheck;
+        boolean secondHoverCheck;
+        String cardClasses;
+
+        hoverOnCard(1);
+        cardClasses = driver.findElement(hoveredFaundOnList).getAttribute("class");
+        firstHoverCheck = cardClasses.contains("b-card_hover");
+        lastHoveredCard = hoveredFaundOnList;
+        methods.Wait(500);
+        hoverOnCard(2);
+        cardClasses = driver.findElement(hoveredFaundOnList).getAttribute("class");
+        secondHoverCheck = cardClasses.contains("b-card_hover") && !(driver.findElement(lastHoveredCard).getAttribute("class").endsWith("b-card_hover"));
+        return firstHoverCheck && secondHoverCheck;
+    }
+
+    public void clickOnFirstCardsHearth() {
+        driver.findElement(hearthCheckbox).click();
+    }
+
+    public void clickOnAddButton() {
+        driver.findElement(addCardButton).click();
+    }
+
+    // пока не делаю, сложности с нахождением локатора для элемента чекбокса на странице
+    public boolean checkHearthChkboxState() {
+        return true;
+    }
+
+    public boolean cardIsSelected (final int cardNum) {
+        String state = driver.findElement(By.cssSelector(getLocatorForCard(cardNum))).getAttribute("class");
+        return state.endsWith("b-card_selected");
+    }
+
+    public boolean checkAddButtonState(final String expectedResult) {
+            String actualResult = driver.findElement(addCardButton).getText();
+            boolean dd = expectedResult.contentEquals(actualResult);
+            return expectedResult.contentEquals(actualResult);
+    }
+
+    public boolean checkLogoCount(final int expectedResult) {
+            int actualCount = Integer.valueOf(driver.findElement(cardsCountOnLogo).getText());
+            return expectedResult == actualCount;
+    }
+
+    public boolean isLogoCountDisplayed() {
+        return driver.findElement(cardsCountOnLogo).isDisplayed();
+    }
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public boolean clickOnFirstFaund () {
+        //запоминаем id первого фонда в списке и кликаем по этому фонду
+        methods.Wait(1000);
+        firstFaundsId = driver.findElement(By.cssSelector(getLocatorForCard(1))).getAttribute("href");
+        clickOnCard(1);
+//        firstFaundsId = driver.findElement(firstFaundOnList).getAttribute("href");
+//        driver.findElement(firstFaundOnList).click();
+        return driver.getCurrentUrl().contains(firstFaundsId);
+    }
+
+    public boolean clickOnFirstDirection() {
+        boolean correctCardsCountOnLogo;
+        String currentCount;
+        methods.Wait(1500);
+        // добавляем фонд в список фондов
+        driver.findElement(addCardButton).click();
+        // проверка что кнопка позеленела и поменялась ???...
+
+        methods.Wait(1000);
+        // проверяем что счетчик в лого = 1
+        currentCount = driver.findElement(cardsCountOnLogo).getText();
+
+        // запоминаем id первого направления и переходим на страницу этого направления, (в атрибуте лежит весь URL)
+        firstDirectionId = driver.findElement(firstDirection).getAttribute("href");
+        driver.findElement(firstDirection).click();
+        methods.Wait(1000);
+        return (driver.getCurrentUrl().equals(firstDirectionId)) &&  "1".equals(currentCount)/*currentCount.equals("1") /*"1".equals(currentCount) correctCardsCountOnLogo*/;
+    }
+
+    public boolean findSelectedFaund() {
+        boolean correctIdFlag;
+
+        methods.Wait(2000);
+        correctIdFlag = firstFaundsId.contains(driver.findElement(selectedFaund).getAttribute("href"));
+        return (driver.findElement(selectedFaund).isDisplayed() && correctIdFlag);
+    }
+
+    public boolean addDirection() {
+        int currentCountInLogo;
+        int currentCountOnBanner;
+        boolean correctButtonText;
+
+        methods.Wait(1000);
+        List<WebElement> cards = driver.findElements(cardContent);
+        driver.findElement(addDirectionButton).click();
+        methods.Wait(700);
+        driver.findElement(goOnPopup).click();
+        methods.Wait(700);
+
+        //проверяем, что       значение_в_счетчике_баннера = значение_в_счетчике_лого - 1       НАВЕРНОЕ ЭТО НЕ НУЖНО ПРОВЕРЯТЬ ПОТОМУ ЧТО ВДРУГ В НАПРАВЛЕНИИ 1 ФОНД
+        currentCountInLogo = Integer.valueOf(driver.findElement(cardsCountOnLogo).getText()).intValue();
+        currentCountOnBanner = Integer.valueOf(driver.findElement(cardsCountOnHeader).getText().replaceAll("[^0-9]", "")).intValue();
+        final int delta = currentCountInLogo - currentCountOnBanner;
+        // и то, что кнопка поменялась на Убрать направление
+        correctButtonText = driver.findElement(addCardButton).getText().equals("Убрать направление");
+        System.out.println(delta == 1);
+        System.out.println(correctButtonText);
+        System.out.println(cards.size());
+        // и то, что количество карточек равно значению_счетчка_в_лого
+        return (delta == 1) &&   cards.size() == currentCountInLogo   &&  correctButtonText;
+    }
+
+    public boolean deleteDirection() {
+        return true;
+    }
+
+    public boolean clickOnLogo() {
+        return true;
+    }
+
+
 
 
 
--- java/Tests/Site/CardsPageTest.java
+++ java/Tests/Site/CardsPageTest.java
@@ -15,10 +15,40 @@ public class CardsPageTest extends SetDriver {
         CardsPage cardsPage = new CardsPage();
         AdditionalMethods methods = new AdditionalMethods();
 
-        getUrl.getSiteUrlWithStr("#card?id=106");
-        methods.Wait(1000);
+        getUrl.getSiteUrlWithStr("#list?category=funds");
 
-        Assert.assertTrue(cardsPage.clickOnAddButton());
+        Assert.assertTrue("Некорректно работает ховер карточек", cardsPage.checkHoverOnFaund());
+
+        // по средечку на странице всех фондов не кликаем
+//        cardsPage.clickOnFirstCardsHearth();
+//        methods.Wait(500);
+//        Assert.assertTrue("Карточка не выделилась при выставлении чекбокса-сердечка", cardsPage.cardIsSelected(1));
+
+        // кликаем на первый фонд
+        cardsPage.clickOnCard(1);
+        methods.Wait(1500);
+
+        // проверяем, что открылся корректный фонд, счетчик в лого корректный и состояние кнопки Добавить тоже корректное
+        Assert.assertTrue("Открылся не тот фонд", cardsPage.idIsMatched(driver.getCurrentUrl(), cardsPage.getLastCardId()));
+        Assert.assertTrue("Неверное состояние кнопки", cardsPage.checkAddButtonState("Добавить фонд"));
+        Assert.assertTrue("Неверный счетчик в лого", cardsPage.checkLogoCount(1));
+        // Меняем состояние кнопки (убираем фонд)
+        cardsPage.clickOnAddButton();
+        methods.Wait(1000);
+        Assert.assertTrue("Неверное состояние кнопки", cardsPage.checkAddButtonState("Добавить фонд"));
+        // логотип в лого скрыт
+        Assert.assertTrue("счетчик в лого не скрыт", cardsPage.isLogoCountDisplayed());
+
+
+
+
+
+        //cardsPage.clickOnCard(1);
+
+        //Assert.assertTrue("Переход на страницу не того фонда", cardsPage.clickOnFirstFaund());
+        //Assert.assertTrue("Переход на страницу не того направления или неверный счетчик в лого", cardsPage.clickOnFirstDirection());
+        //Assert.assertTrue("Выбранный фонд не найден на странице направления", cardsPage.findSelectedFaund());
+        //Assert.assertTrue("Кнопка не поменялась на Убрать направление или неверный счетчик", cardsPage.addDirection());
     }
 
 
